3c9504077bf88a251e902114b6e3bcb9b1718f0a
Ollynov <ollynov@gmail.com>
2023-04-27 14:57:19 -0700

    starting work on carousel non-interactive

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,5 +1,23 @@
+import image1 from "./../Images/verkada1.png";
+import image2 from "./../Images/verkada2.png";
+import image3 from "./../Images/verkada3.png";
+
 const Carousel = () => {
-  return <div className="w-[518px] bg-green-700 h-full"></div>;
+  return (
+    <div className="w-[518px] bg-green-700 h-full relative">
+      <img src={image1} className="mt-[60px] fixed rounded-lg" alt="logo" />
+      <img
+        src={image2}
+        className="absolute bottom-0 right-[41px] rounded-lg"
+        alt="logo"
+      />{" "}
+      <img
+        src={image3}
+        className="absolute top-0 right-0 rounded-lg"
+        alt="logo"
+      />
+    </div>
+  );
 };
 
 export default Carousel;
